20fee985b3c2029ba8f54ebab6d4954af8d7f3c9
Mona Lisa <roldansebasdaza@gmail.com>
2023-10-18 01:05:36 -0500

    SRD

--- CS-PR1/procesar_domicilio.php
+++ CS-PR1/procesar_domicilio.php
@@ -10,7 +10,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
-<body style="background-repeat: no-repeat; background-size: cover; background-image: url(IMG/Fondos/foto\ 7.jpeg);">
+<body style="background-repeat: no-repeat; background-size: cover; background-image: url(IMG/Fondos/foto\ 4.jpg);">
+
 <nav class="navbar">
         <div class="logo">
         <a href="INDEX.php" target="_self"><img src="IMG/logos/creativestar3.png" alt="Logo"></a>
@@ -33,89 +34,12 @@
         <div class="icono">
           <a href="QSOMOS.php" target="_self"><img src="IMG/Fondos/brocha.png"></a>
         </div>
-</nav>
-<!---------------------->
-<style>
-   form {
-      width: 100%;
-      max-width: 500px; 
-      background-color: #55C9F6;
-      padding: 20px;
-      box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-      border-radius: 5px;
-    }
-
-    h1 {
-      text-align: center;
-      color: black;
-    }
-
-    label {
-      display: block;
-      margin-bottom: 10px;
-      color: #333;
-    }
-
-    input[type="text"],
-    input[type="tel"],
-    input[type="email"],
-    textarea {
-      width: 100%;
-      padding: 10px;
-      margin-bottom: 20px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-    }
-
-    input[type="submit"] {
-      padding: 10px 20px;
-      background-color: #3498DB;
-      color: white;
-      border: none;
-      border-radius: 5px;
-      cursor: pointer;
-    }
-
-    input[type="submit"]:hover {
-      background-color: #2980B9;
-    }
-    @media (max-width: 600px) {
-      form {
-        max-width: 100%;
-      }
-    }
-  </style>
-</head>
-<body>
-  <center>
-  <br>
-  <br>
-  <form action="procesar_domicilio.php" method="POST">
-    <h1>Formulario de Toma de Domicilios</h1>
-    <label for="nombre">Nombre Completo:</label>
-    <input type="text" id="nombre" name="nombre" required>
-
-    <label for="telefono">Teléfono de Contacto:</label>
-    <input type="tel" id="telefono" name "telefono" required>
-
-    <label for="correo">Correo Electrónico:</label>
-    <input type="email" id="correo" name="correo" required>
-
-    <label for="direccion">Dirección de Entrega:</label>
-    <textarea id="direccion" name="direccion" rows="4" required></textarea>
-
-    <label for="pedido">Detalle del Pedido:</label>
-    <textarea id="pedido" name="pedido" rows="4" required></textarea>
-
-    <input type="submit" value="Enviar Pedido">
-  </form>
-  <br>
-  <br>
-  <br>
-  </center>
-<!---------------------->
-
+      </nav>
+<center>
+      <h1>SE PROCESO CON EXITO TU DOMICILIO :D</h1>
+</center>
 </body>
+
 <footer class="footer">
 
   <div class="shared-panel">
@@ -127,3 +51,5 @@
   </div>
 
   </footer>
+
+</html>
