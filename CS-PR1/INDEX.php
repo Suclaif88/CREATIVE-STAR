b4715962c7f33a1be444b6a228b683601ed6983e
Mona Lisa <roldansebasdaza@gmail.com>
2023-10-17 21:14:31 -0500

    SRD

---
+++ CS-PR1/INDEX.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CREATIVE STAR</title>
+    
+    <link rel="stylesheet" href="ESTILOS/ESTILOS.css">
+    <link rel="stylesheet" href="ESTILOS/boostrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+<body>
+
+</body>
+
+<footer class="footer">
+
+  <div class="shared-panel">
+	<a target="_blank" class="shared fb" title="Facebook" href="https://m.facebook.com/login/?locale=es_ES"></a>	
+	<a target="_blank" class="shared tt" title="Twitter" href="https://twitter.com/?lang=es"></a>
+	<a target="_blank" class="shared gp" title="Google+" href="https://myaccount.google.com/profile"></a>
+	<a target="_blank" class="shared wa" title="WhatsApp" href="https://www.whatsapp.com/?lang=es"></a>
+	<a target="_blank" class="shared ce" id="sml" title="Email" href="https://www.google.com/intl/es-419/gmail/about/"></a>		
+  </div>
+
+  </footer>
+
+</html>
